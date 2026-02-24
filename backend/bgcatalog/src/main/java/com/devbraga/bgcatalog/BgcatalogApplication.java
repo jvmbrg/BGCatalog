8762cbb344a0838b90f95bd83f0c734c76d0ca6e
jvmbrg <jvitormbraga@gmail.com>
2026-02-24 17:10:49 -0300

    first commit

---
+++ backend/bgcatalog/src/main/java/com/devbraga/bgcatalog/BgcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.devbraga.bgcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgcatalogApplication.class, args);
+	}
+
+}
